e1ab11aebf6698297dab59e5435b36e58b076a0d
linyork <jason945119@gmail.com>
2017-09-19 15:41:40 +0800

    0919
    0919

--- WebPush.php
+++ WebPush.php
@@ -6,25 +6,27 @@
 class WebPush
 {
     // Construct parameters
-    private $type_array = array('fcm', 'safari');
-    private $times, $type, $errorMsg, $status;
+    private $typeList = array('fcm', 'safari');
+    private $times, $type, $errorMsg;
     // Fcm parameters
-    private $xxx;
+    private $fcmApiAccessKey, $timeToLive;
     // Safari parameters
-    private $expiry, $certificateFile, $passphrase;
-    // Package
-    private $title, $body, $icon, $url, $button;
+    private $expiryTime, $certificateFile, $passPhrase;
+    // Token
+    private $token;
+    // Fcm Package
+    private $payloadData;
+    // Safari Package
+    private $title, $body, $icon, $url;
 
     /**
      * @param string $type
      * @param array  $array
-     *
-     * @return TRUE | FALSE
      */
     function __construct( $type, $array )
     {
         # 判斷是否在list裡
-        if ( in_array($type, $this->type_array) )
+        if( in_array( $type, $this->typeList ) )
         {
             $this->type = $type;
         }
@@ -32,14 +34,40 @@ class WebPush
         {
             $this->errorMsg = "Undefined type: {$type}";
         }
-        if ( ! empty( $this->errorMsg ) )return $this->status;
 
-        # 判斷所需參數是否都有設定
-        foreach ($array as $key => $value)
+        # 驗證該 type的array是否都有正確key值
+        $this->verifyParameters( $type, $array );
+
+        # 判斷所需參數是否都有值
+        foreach ( $array as $key => $value )
         {
             $this->verify( $key, $value );
         }
-        if ( ! empty( $this->errorMsg ) )return $this->status;
+    }
+
+    /**
+     * @param string $token
+     * @param array  $payloadData
+     *
+     * @return TRUE | FALSE
+     */
+    public function webPushFcm( $token = FALSE, $payloadData = FALSE )
+    {
+        # type是否有錯誤
+        if( $this->type != "fcm")
+        {
+            $this->errorMsg = "Use the wrong function";
+            return FALSE;
+        }
+        # 檢查物件是否有錯誤
+        if( ! empty( $this->errorMsg ) ) return FALSE;
+        # 判斷所需參數是否都有設定
+        $this->verify( "token", $token );
+        $this->verify( "payloadData", $payloadData );
+        if( ! empty( $this->errorMsg ) )return FALSE;
+
+        # Fcm 推播
+        return $this->sendPushFcm( $this->token, $this->payloadData );
     }
 
     /**
@@ -47,36 +75,30 @@ class WebPush
      * @param string $title
      * @param string $body
      * @param array  $url
-     * @param string $icon
-     * @param array  $button
      *
      * @return TRUE | FALSE
      */
-    function __invoke( $token = FALSE, $title = FALSE, $body = FALSE, $url = FALSE, $icon = FALSE, $button = FALSE )
+    public function webPushSafari( $token = FALSE, $title = FALSE, $body = FALSE, $url = FALSE )
     {
+        # type是否有錯誤
+        if( $this->type != "safari")
+        {
+            $this->errorMsg = "Use the wrong function";
+            return FALSE;
+        }
+        # 檢查物件是否有錯誤
+        if( ! empty( $this->errorMsg ) ) return FALSE;
         # 判斷所需參數是否都有設定
         $this->verify( "token", $token );
         $this->verify( "title", $title );
         $this->verify( "body", $body );
         $this->verify( "url", $url );
-        if ( ! empty( $this->errorMsg ) )return $this->status;
+        if( ! empty( $this->errorMsg ) )return FALSE;
 
-        switch ($this->type)
-        {
-            case 'fcm':
-                $this->verify( "icon", $icon );
-                if ( ! empty( $this->errorMsg ) )return $this->status;
-                $this->sendPushFcm($token, $title, $body, $url, $icon, $button);
-                return $this->status;
-                break;
-            
-            case 'safari':
-                $this->sendPushSafari($token, $title, $body, $url);
-                return $this->status;
-                break;
-        }
+        # Safari 推播
+        return $this->sendPushSafari( $this->token, $this->title, $this->body, $this->url );
     }
-    
+
     /**
      * @return string
      */
@@ -85,91 +107,168 @@ class WebPush
         return $this->errorMsg;
     }
 
+    /* 驗證Fcm Safari參數 */ 
+    private function verifyParameters( $type, $array )
+    {
+        switch( $type )
+        {
+            case 'fcm':
+                ! array_key_exists( "fcmApiAccessKey", $array ) && $this->errorMsg = "fcmApiAccessKey key does not exist.";
+                ! array_key_exists( "timeToLive", $array ) && $this->errorMsg = "timeToLive key does not exist.";
+                break;
+            case 'safari':
+                ! array_key_exists( "expiryTime", $array ) && $this->errorMsg = "expiryTime key does not exist.";
+                ! array_key_exists( "certificateFile", $array ) && $this->errorMsg = "certificateFile key does not exist.";
+                ! array_key_exists( "passPhrase", $array )  && $this->errorMsg = "passPhrase key does not exist.";
+                ! file_exists( $array['certificateFile'] ) && $this->errorMsg = "Undefined certificateFile.";
+                break;
+        }
+    }
+
     /* 驗證 */ 
     private function verify( $type, $para )
     {
         try
         {
-            if(!empty($para))
+            if( ! empty( $para ) )
             {
                 $this->$type = $para;
             }
-            else throw new Exception("Undefined variable: {$type}");
+            else throw new Exception( "Invalid variable: {$type}" );
         }
-        catch(Exception $e)
+        catch( Exception $e )
         {
             $this->errorMsg = $e->getMessage();
-            $this->status = FALSE;
         }
     }
 
     /* Fcm 推播 */
-    private function sendPushFcm( $token, $title, $body, $url, $icon , $button = FALSE )
+    private function sendPushFcm( $token, $payloadData )
     {
-        // FCM Server Production
-        $google_server_url = 'https://android.googleapis.com/gcm/send';
+        # Google Server 網址
+        $google_server_url = 'https://fcm.googleapis.com/fcm/send';
 
+        # Request 標頭
+        $headers = array(
+            'Authorization: key=' . $this->fcmApiAccessKey,
+            'Content-Type: application/json',
+        );
+
+        # Post
+        $fields = array( 
+            "data" => $payloadData,
+            "registration_ids" => array( $token ),
+            'time_to_live' => $this->timeToLive,
+        );
+
+        # curl 設定
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $google_server_url );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE) ;
+        curl_setopt( $ch, CURLOPT_POST, TRUE );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $fields ) );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+
+        # 回傳結果處理
+        $result = curl_exec( $ch );
+        if( $result === FALSE )
+        {
+            $this->errorMsg = "Curl failed ".curl_error( $ch );
+            return FALSE;
+        }
+        else
+        {
+            # 失效原因清單 => 失效 ／ 此token不是這個的專案SenderId訂閱的 ／ 錯誤的token
+            $invalid_reasons = array( 'NotRegistered', 'MismatchSenderId', 'InvalidRegistration' );
+
+            # 回傳的結果 error 有在失效原因清單內 標記 invalid
+            $result = json_decode( $result, TRUE );
+            if( is_array( $result ) )
+            { 
+                $results = $result['results'];
+                foreach( $results as $key => $row ) 
+                {
+                    # 失敗回傳的key == "error"
+                    if( array_key_exists('error', $row) )
+                    {
+                        if( in_array($row['error'], $invalid_reasons ) )
+                        {
+                            $this->errorMsg = $row['error'];
+                            return FALSE;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                $this->errorMsg = "Curl failed Result is not array";
+                return FALSE;
+            }
+        }
+        # 關閉curl連線
+        curl_close( $ch );
+        return TRUE;
     }
 
     /* Safari 推播 */
     private function sendPushSafari( $token, $title, $body, $url )
     {
 
-        # APNS Server Production
+        # APNS Server 網址
         $pushServer = 'ssl://gateway.push.apple.com:2195';
         $feedbackServer = 'ssl://feedback.push.apple.com:2196';
 
         # Create Stream config
         $streamContext = stream_context_create();
         stream_context_set_option( $streamContext, 'ssl', 'local_cert', $this->certificateFile );
-        stream_context_set_option( $streamContext, 'ssl', 'passphrase', $this->passphrase );
+        stream_context_set_option( $streamContext, 'ssl', 'passphrase', $this->passPhrase );
 
         # Create Stream Connection
         $fp = stream_socket_client( $pushServer, $errorCode, $errorStr, 100, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $streamContext );
 
         if( ! $fp )$this->errorMsg = "Stream socket client failed.";
-        if ( ! empty( $this->errorMsg ) )return $this->errorMsg;
+        if( ! empty( $this->errorMsg ) )return $this->errorMsg;
 
         # This allows fread() to return right away when there are no errors.
-        stream_set_blocking ($fp, 0);
+        stream_set_blocking ( $fp, 0 );
 
         # payload
         $payload = array(
             'aps' => array(
                 'alert' => array(
-                    'title' => $this->title,
-                    'body'=> $this->body,
+                    'title' => $title,
+                    'body'=> $body,
                     'action'=> 'view',
                 ),
-                'url-args' => $this->url
+                'url-args' => $url
             )
         );
-        $json_payload = json_encode($payload);
+        $json_payload = json_encode( $payload );
 
         # Enhanced Notification
-        $binary = pack('CNNnH*n', 1, 1, $expiry, 32, trim($token), strlen($json_payload)).$json_payload;
+        $binary = pack( 'CNNnH*n', 1, 1, $this->expiryTime, 32, trim( $token ), strlen( $json_payload ) ) . $json_payload;
         # 如果發送失敗，則進行重發，連續３次失敗，則重新建立連接，並從下一個 DeviceToken 發送
         # 如果發送成功，但 token 回傳 Error 則中止當前推播，標記invalid，使用遞迴，從下一個Token重啟推播 
         $times = 1;
-        while($times <= 3)
+        while ( $times <= 3 )
         {
             # 發送 or 重發
-            $result = fwrite($fp, $binary);
+            $result = fwrite( $fp, $binary );
             # 發送失敗
-            if (!$result)
+            if( ! $result )
             {
-                if ($times = 3)
+                if( $times = 3 )
                 {
                     # 重新建立連接
-                    fclose($fp);
-                    $fp = stream_socket_client($pushServer, $errorCode, $errorStr, 100, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $streamContext);
-                    if(!$fp)
+                    fclose( $fp );
+                    $fp = stream_socket_client( $pushServer, $errorCode, $errorStr, 100, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $streamContext );
+                    if( ! $fp )
                     {
                         $this->errorMsg = "Stream socket client failed.";
-                        $this->status = FALSE;
                         return FALSE;
                     }
-                    stream_set_blocking ($fp, 0);
+                    stream_set_blocking ( $fp, 0 );
                 }
                 $times++;
                 continue;
@@ -178,89 +277,86 @@ class WebPush
             else 
             {
                 # 等待傳送結果 0.5秒
-                usleep(500000);
+                usleep( 500000 );
                 # 傳送結果
-                $error_key = $this->checkAppleErrorResponse($fp);
-                if ($error_key === "success") 
+                $error_key = $this->checkAppleErrorResponse( $fp );
+                if( $error_key === "success" ) 
                 {
-                    # 記錄推播狀態
-                    $this->status = $error_key;
                     # 結束 發送三次的 while 迴圈
                     break;
                 }
                 else
                 {
                     # 關閉APNs連接
-                    fclose($fp);
+                    fclose( $fp );
                     # 記錄推播狀態
                     $this->errorMsg = $error_key;
-                    $this->status = FALSE;
                     return FALSE;
                 }
             }
         }
         # 關閉APNs連接
-        fclose($fp);
+        fclose( $fp );
         return TRUE;
     }
 
     /* 檢查APNS Error Response */
-    private function checkAppleErrorResponse(&$fp)
+    private function checkAppleErrorResponse( &$fp )
     {
         # byte1=always 8, byte2=StatusCode, bytes3,4,5,6=identifier(rowID). Should return nothing if OK.
         $apple_error_response = fread($fp, 6);
         # NOTE: Make sure you set stream_set_blocking($fp, 0) or else fread will pause your script and wait forever when there is no response to be sent.
 
-        if ($apple_error_response) 
+        if($apple_error_response) 
         {
             # unpack the error response (first byte 'command" should always be 8)
             $error_response = unpack('Ccommand/Cstatus_code/Nidentifier', $apple_error_response);
 
-            if ($error_response['status_code'] == '0')
+            if( $error_response['status_code'] == '0' )
             {
                 $error_response['status_code'] = '0-No errors encountered';
             }
-            else if ($error_response['status_code'] == '1')
+            else if( $error_response['status_code'] == '1' )
             {
                 $error_response['status_code'] = '1-Processing error';
             }
-            else if ($error_response['status_code'] == '2')
+            else if( $error_response['status_code'] == '2' )
             {
                 $error_response['status_code'] = '2-Missing device token';
             }
-            else if ($error_response['status_code'] == '3')
+            else if( $error_response['status_code'] == '3' )
             {
                 $error_response['status_code'] = '3-Missing topic';
             }
-            else if ($error_response['status_code'] == '4')
+            else if( $error_response['status_code'] == '4' )
             {
                 $error_response['status_code'] = '4-Missing payload';
             }
-            else if ($error_response['status_code'] == '5')
+            else if( $error_response['status_code'] == '5' )
             {
                 $error_response['status_code'] = '5-Invalid token size';
             }
-            else if ($error_response['status_code'] == '6')
+            else if( $error_response['status_code'] == '6' )
             {
                 $error_response['status_code'] = '6-Invalid topic size';
             }
-            else if ($error_response['status_code'] == '7')
+            else if( $error_response['status_code'] == '7' )
             {
                 $error_response['status_code'] = '7-Invalid payload size';
             }
-            else if ($error_response['status_code'] == '8')
+            else if( $error_response['status_code'] == '8' )
             {
                 $error_response['status_code'] = '8-Invalid token';
             }
-            else if ($error_response['status_code'] == '10')
+            else if( $error_response['status_code'] == '10' )
             {
                 $error_response['status_code'] = '10-Shutdown';
             }
-            else if ($error_response['status_code'] == '128')
+            else if( $error_response['status_code'] == '128' )
             {
                 $error_response['status_code'] = '128-Protocol error (APNs could not parse the notification)';
             }
-            else if ($error_response['status_code'] == '255')
+            else if( $error_response['status_code'] == '255' )
             {
                 $error_response['status_code'] = '255-None (unknown)';
             }
@@ -272,7 +368,7 @@ class WebPush
             /*
                 Identifier is the rowID (index) in the database that caused the problem, and Apple will disconnect you from server. To continue sending Push Notifications, just start at the next rowID after this Identifier.
             */
-            return $error_response['identifier'];
+            return $error_response['status_code'];
         }
         return "success";
     }
